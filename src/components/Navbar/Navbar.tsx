8c1bec78d0fe18ff1a340938c44323b7908ef712
Kim Pham <km.phm2@gmail.com>
2024-04-21 00:52:14 +1000

    create main content ui

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Settings } from './Settings';
 
 export const Navbar = () => (
-    <nav className="flex px-[96px] py-[38px] font-black text-[32px]">
+    <nav className="flex px-[96px] py-[38px] font-black text-[32px] z-20">
         <h1 className="w-full"><a href="/">AC Hourly Music</a></h1>
 
         <div className="flex justify-end gap-[61px] self-center overflow-hidden w-full">
