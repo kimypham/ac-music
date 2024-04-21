2cacf6d7f90c38c33afab8742b1824f956cd3c0e
Kim Pham <km.phm2@gmail.com>
2024-04-21 18:01:33 +1000

    fix footer text for light mode

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <div className="flex-col z-10 text-[14px] my-[24px] h-fit self-center text-center">
+        <div className="flex-col z-10 text-[14px] my-[24px] h-fit self-center text-center text-lm-text-dark-blue">
             <p className="font-black">
                 Made with ♥ by Kim Pham - View code on <a className="link" href="https://github.com/kimypham/ac-music" target="_blank">Github</a> - Questions/comments? <a className="link" href="mailto:km.phm2@gmail.com">Contact me!</a>
             </p>
