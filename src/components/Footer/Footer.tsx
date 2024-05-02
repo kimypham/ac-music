fe42728fe3b910a096f8431c97e2bb5f99a63831
Kim Pham <km.phm2@gmail.com>
2024-05-02 23:04:06 +1000

    change footer UI to redesign, fix link underline

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,12 @@
 export const Footer = () => {
     return (
-        <div className="flex-col z-10 text-[14px] my-[24px] h-fit self-center text-center">
-            <p className="font-black">
+        <div className="flex-col z-10 text-[12px] my-[24px] h-fit self-center text-center">
+            <p className="font-bold">
                 Made with ♥ by Kim Pham - View code on <a className="link" href="https://github.com/kimypham/ac-music" target="_blank">Github</a> - Questions/comments? <a className="link" href="mailto:km.phm2@gmail.com">Contact me!</a>
             </p>
-            <p className="font-bold">
-                This website is an independent platform and is not associated with, validated, backed, or sponsored by Animal Crossing or Nintendo.
-            </p>
-            <p className="font-bold">
-                Animal Crossing is copyrighted and is a trademark of Nintendo. I do not own any music used on this site.
+            <p>
+                This website is an independent platform and is not associated with, backed, or sponsored by Animal Crossing or Nintendo.<br />
+                Animal Crossing is copyrighted and is a trademark of Nintendo. I do not own any music or sounds used on this site.
             </p>
         </div >
     );
