9396676dd12075fa71d2b0c179434e92b8429f4e
Kim Pham <km.phm2@gmail.com>
2024-05-11 15:38:48 +1000

    add functional clock back in

--- src/components/MainText/MainText.tsx
+++ src/components/MainText/MainText.tsx
@@ -1,6 +1,13 @@
 import { PropsWithChildren } from 'react';
+import { useTime } from '../../hooks';
+import { getHourString } from './MainText.service';
+import { FormattedHour } from '../../common';
 
 export const MainText = () => {
+    const time: Date = useTime();
+    const hourString: FormattedHour = getHourString(time);
+    const timeString: string = time.toLocaleTimeString(['en-AU'], { timeStyle: 'short' }).split(' ').join('');
+
     const HighlightText = ({ children }: PropsWithChildren) => {
         return <div className="text-lm-text-blue dark:text-dm-text-green inline">
             {children}
@@ -9,8 +16,8 @@ export const MainText = () => {
 
     return (
         <p className="font-extrabold leading-loose">
-            It's currently <HighlightText>2:12pm</HighlightText> and <HighlightText>cloudy</HighlightText>!<br />
-            Now playing: <HighlightText>2PM AC: NH Soundtrack</HighlightText>
+            It's currently <HighlightText>{timeString}</HighlightText> and <HighlightText>cloudy</HighlightText>!<br />
+            Now playing: <HighlightText>{hourString} AC: NH Soundtrack</HighlightText>
         </p>
     )
 }
