db2fe0841b735e75e0075c813e89ac4a728fac1c
Kim Pham <km.phm2@gmail.com>
2024-07-28 01:07:13 +1000

    feat: change text based on soundtrack selected

--- src/components/MainText/MainText.tsx
+++ src/components/MainText/MainText.tsx
@@ -1,12 +1,28 @@
 import { PropsWithChildren } from 'react';
-import { FormattedHour } from '../../common';
+import { FormattedHour, GameSoundtrackLabel, GameSoundtrackValue } from '../../common';
 import { useTime } from '../../hooks';
-import { getHour } from '../../common/service';
+import { getHour, getSettingsFromLocalStorage } from '../../common/service';
 
 export const MainText = () => {
     const time: Date = useTime();
     const timeString: string = time.toLocaleTimeString(['en-AU'], { timeStyle: 'short' }).split(' ').join('');
     const hour: FormattedHour = getHour(time);
+
+    const getLabelFromValue = (value: GameSoundtrackValue): GameSoundtrackLabel => {
+        const valueToLabelMap = {
+            [GameSoundtrackValue.Original]: GameSoundtrackLabel.Original,
+            [GameSoundtrackValue.WWCF]: GameSoundtrackLabel.WWCF,
+            [GameSoundtrackValue.NL]: GameSoundtrackLabel.NL,
+            [GameSoundtrackValue.NH]: GameSoundtrackLabel.NH,
+            [GameSoundtrackValue.Random]: GameSoundtrackLabel.Random,
+        };
+
+        return valueToLabelMap[value];
+    };
+
+    const soundtrackValue: GameSoundtrackValue = getSettingsFromLocalStorage().gameSoundtrack;
+    const soundtrack: GameSoundtrackLabel = getLabelFromValue(soundtrackValue);
+
 
     const HighlightText = ({ children }: PropsWithChildren) => {
         return <div className="text-lm-text-blue dark:text-dm-text-green inline">
@@ -17,7 +33,7 @@ export const MainText = () => {
     return (
         <p className="font-extrabold leading-loose">
             It's currently <HighlightText>{timeString}</HighlightText> and <HighlightText>cloudy</HighlightText>!<br />
-            Now playing: <HighlightText>{hour} AC: NH Soundtrack</HighlightText>
+            Now playing: <HighlightText>{hour} AC: {soundtrack} Soundtrack</HighlightText>
         </p>
     )
 }
