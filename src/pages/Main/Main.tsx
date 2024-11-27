189e5aeda2f3e184d2a7632caf63919b88e8d585
Kim Pham <km.phm2@gmail.com>
2024-11-28 02:01:11 +1100

    feat: prepare for deployment - remove in-progress features, fix github pages deployment

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,14 +1,14 @@
-import { MainText, Video, VideoControls, VideoSettings } from '../../components';
+import { MainText, Video, VideoSettings } from '../../components';
 
 export const Main = () => {
     return (
         <div className="flex px-[96px] font-bold font-open-sans text-[32px] text-lm-brown text-center dark:text-dm-white self-center my-auto w-full h-full">
             <div className="flex-col bg-[#FFF9F5] dark:bg-dm-bg-green w-full h-full rounded-[30px] justify-center content-center">
                 <MainText />
-                <div className="flex mt-[32px] place-content-evenly">
-                    <div>
+                <div className="flex flex-col place-content-evenly items-center">
+                    <div className="my-[20px]">
                         <Video />
-                        <VideoControls />
+                        {/* <VideoControls /> */}
                     </div>
                     <VideoSettings />
                 </div>
